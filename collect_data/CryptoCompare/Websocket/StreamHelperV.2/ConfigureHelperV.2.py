be69aa391944126fb0c08d5438da5047d9d4812f
carv-cmd <bencarv1324@gmail.com>
2021-04-02 11:17:53 -0400

    coinWizard otw

--- collect_data/CryptoCompare/Websocket/StreamHelperV.2/ConfigureHelperV.2.py
+++ collect_data/CryptoCompare/Websocket/StreamHelperV.2/ConfigureHelperV.2.py
@@ -1,24 +1,28 @@
+from pymongo import MongoClient as Mcl
 from dotenv import load_dotenv, find_dotenv
 import logging as logs
 import os
 import requests
-import bson
+import time
+from pprint import pprint
 
+line = '\n' \
+       '--------------------------------------------------------------------------' \
+       '\n'
 load_dotenv(find_dotenv())
 apiKey = os.getenv('CCOMPARE_DATA_API_KEY')
-
 logs.basicConfig(filename='BSON_fileSize.log', filemode='w', level=logs.INFO)
 
 
 class PrepareRequest(dict):
-    """ Descriptor class for CryptoCompare REST API calls """
+    """ Descriptor class for CryptoCompare REST-helper-endpoints wrapper """
     
     def __init__(self, limit=10, page=0, fiat='USD', asset_class='ALL'):
         super(PrepareRequest, self).__init__()
         self.requestData = dict(limit=limit, page=page, tsym=fiat, asset_class=asset_class, api_key=apiKey)
     
     def __setattr__(self, key, value):
-        print(f">>> Setting '{key}'...")
+        print(f">> Setting.PrepareRequest({key}). . .")
         super().__setattr__(key, value)
         
     def __repr__(self):
@@ -41,55 +45,88 @@ class TopLists:
         self.parameters = vars(PrepareRequest(**kwargs))
     
     def __getitem__(self, item):
-        print(f">>> Getting '{item}'")
+        print(f">> Getting.TopLists({item}). . .")
         return self.__dict__[item]
-        
-    def set_url(self, change_url):
-        self.url = self.top_end[change_url]
         
     def __repr__(self):
         return f'\nTargetURL: {self.url}\n' \
                f'\nRequestParameters: {self.parameters}\n'
 
 
-def send_wrapper():
-    
-    def send_requests(new_endpoint='TopPercentChange'):
-        foobar = TopLists(limit=10)
-        req_url = foobar['url']
-        req_parameters = foobar['parameters']['requestData']
+def send_requests(metrics):
+    """ Wrapper to initiate """
+    def response_object(**sendto):
+        response_dict = dict()
         
-        def to_bson(json_elem):
-            bson_obj = bson.encode(json_elem)
-            logs.info(f"\nResponse From: {req_url}"
-                      f"\nMAX byte count for BSON document is (160,000,000bytes) or (16MB)!"
-                      f"\n\tCurrent BSON Representation Byte Count: ({len(bson_obj)}-rawBytes)"
-                      f"\n\t\tApprox Document Size:\n\t\t\t({len(bson_obj) / 1000000}MB / 16MB)\n"
-                      f"\n\n{bson_obj}\n\n")
-            return bson_obj
-        
-        def response_object():
-            foobar.set_url(new_endpoint)
+        for report, passing in sendto.items():
             try:
-                return to_bson(requests.get(req_url, params=req_parameters).json())
-            except TypeError or KeyError or ValueError:
-                raise
+                # TODO :MongoDB: dict(metric=report, data=requests.get(passing[0], params=passing[1]).json())
+                print(f">>> Attempting Request:({report}):\n\tFrom: '{passing[0]}'")
+                daters = requests.get(passing[0], params=passing[1]).json()
+                response_dict[report] = daters
+                time.sleep(.25)
+            except requests.exceptions.ConnectionError:
+                requests.status_codes = 'Connection refused'
+                print('CONNECTION REFUSED INCREASE DURATION BETWEEN SUBSEQUENT REQUESTS')
+                
+        del response_dict['TopPercentChange']['SponsoredData']
+        return response_dict
+     
+    def sender(**kwargs):
+        sendto = dict()
+        
+        for elem in metrics:
+            foo = TopLists(endpoint=elem, **kwargs)
+            sendto[elem] = [foo['url'], foo['parameters']['requestData']]
+            print(f"\n>>> GENERATING METRICS FOR: {elem}. . .")
+            pprint(sendto[elem])
+            print(line)
+            
+        return response_object(**sendto)
+    return sender
+
+
+def send_agent(**kwargs):
+    """
+    Create a list named 'check_these' in the global name space containing the metrics to request
+    Options: ["TopPercentChange", "TopVolSubs", "TopMktCap", "TopByPrice", "TopDirectVol", "TopTierVolSubs"]
+    :param kwargs: limit=10, page=0, fiat='USD', asset_class='ALL'
+    :return: dictionary object where; key='metric' & value='responseData_FullNames_list"
+    """
+    get_from = send_requests(check_these)
+    return_values = get_from(**kwargs)
+    degens = dict()
     
-        return response_object()
-    
-    return send_requests
-    
+    for ik, iv in return_values.items():
+        deg = []
+        for metric in iv['Data']:
+            deg.append(metric['CoinInfo']['FullName'])
+        degens[ik] = deg
+        
+    print('\n------>>> TASK FAILED SUCCESSFULLY <<<------\n')
+    return degens
+
 
 if __name__ == "__main__":
     print(f"\n>>> Initializing `ConfigureHelperV.2` as '{__name__}'...\n")
-    
-    try:
-        check_all = ["TopVolSubs", "TopTierVolSubs", "TopMktCap", "TopDirectVol", "TopByPrice", "TopPercentChange"]
-        sender = send_wrapper()
-        for metric in check_all:
-            sender(metric)
-        print('\nTask Completed Successfully!\nCheck .log for response size.\n')
+
+    check_these = ["TopPercentChange", "TopVolSubs", "TopMktCap",
+                   "TopByPrice", "TopDirectVol", "TopTierVolSubs"]
+
+    # KWARGS: send_agent(limit = 10, page = 0, fiat = 'USD', asset_class = 'ALL')
+    for ikey, ivalue in send_agent().items():
+        print(f"\n>>> Metric: {ikey}\n"
+              f"\n>>> Data:\n{ivalue}")
         
-    except TypeError or KeyError or ValueError:
-        print('FIX REQUEST METHOD')
-        raise
+else:
+    print(f"\n>>> Initializing `ConfigureHelperV.2` as '{__name__}'...\n")
+    check_these = []
+    send_helper = send_agent
+# def to_bson(json_elem):
+    #     bson_obj = bson.encode(json_elem)
+    #     logs.info(f"\nAttempting Connection w/: {req_url}"
+    #               f"\nMAX byte count for BSON document is (160,000,000bytes) or (16MB)!"
+    #               f"\n\tCurrent BSON Representation Byte Count: ({len(bson_obj)}-rawBytes)"
+    #               f"\n\t\tApprox Document Size:\n\t\t\t({len(bson_obj) / 1000000}MB / 16MB)\n"
+    #               f"\n\n{bson_obj}\n\n")
+    #     return bson_obj
