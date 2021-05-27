9ed3b558ad80e3c89d4fcf5d4ede00d24aae0e0a
carv-cmd <bencarv1324@gmail.com>
2021-05-27 15:35:26 -0400

    Set upstream quick commit

---
+++ GlassNode/GlassMongo.py
@@ -0,0 +1,9 @@
+# class MongoServer:
+#     def __init__(self):
+#         pass
+# TODO Create Mongo class + identifiers of sorts(maybe file handler?)
+#   * Collection/Document Header -> Last Update Datetime
+#   * JSON/BSON fields
+# class MongoClient(MongoBroker):
+#     def __init__(self):
+#         super(MongoClient, self).__init__()
--- GlassNode/GlassNodeBroker.py
+++ GlassNode/GlassNodeBroker.py
@@ -311,16 +311,6 @@ else:
 #   * ('market', 'mvrv_less_155', {'a': "BTC"}),
 #   * ('market', 'mvrv_more_155', {'a': "BTC"}),
 
-# class MongoServer:
-#     def __init__(self):
-#         pass
-# TODO Create Mongo class + identifiers of sorts(maybe file handler?)
-#   * Collection/Document Header -> Last Update Datetime
-#   * JSON/BSON fields
-# class MongoClient(MongoBroker):
-#     def __init__(self):
-#         super(MongoClient, self).__init__()
-
 # _non_standard_responses = (
 #     'futures_funding_rate_perpetual_all',
 #     'futures_volume_daily_sum_all',
